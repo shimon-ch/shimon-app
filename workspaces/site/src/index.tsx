f2eaa78bdba7cf95640f7ac912ca4eef4d237ac7
shimon.ishikawa <shimon.ishikawa@accenture.com>
2024-02-28 00:43:04 +0900

    /site ssg対応

--- workspaces/site/src/index.tsx
+++ workspaces/site/src/index.tsx
@@ -6,7 +6,12 @@ const app = new Hono()
 app.get('*', renderer)
 
 app.get('/', (c) => {
-  return c.render(<h1>Hello!</h1>)
+  return c.render(
+    <h1>Hello! World!</h1>,
+    {
+      title: 'Hello! World!'
+    }
+  )
 })
 
 export default app
--- workspaces/site/vite.config.ts
+++ workspaces/site/vite.config.ts
@@ -1,10 +1,12 @@
 import build from '@hono/vite-cloudflare-pages'
+import ssg from '@hono/vite-ssg'
 import devServer from '@hono/vite-dev-server'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [
     build(),
+    ssg(),
     devServer({
       entry: 'src/index.tsx'
     })
